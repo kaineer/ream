49dc0f702ab3a14eab7fb0c2c655596c04464d7d
Sergey Kluchkowsky <kaineer@gmail.com>
2009-11-14 22:29:00 +0500

    Renamed template_cache__.rb

--- lib/ream/blog/template_cache.rb
+++ lib/ream/blog/template_cache.rb
@@ -1,66 +1,83 @@
 #
 #
 #
-#
-#
 
 require 'ream/template'
 
+require File.join( File.dirname( __FILE__ ), "template_processor" )
+require File.join( File.dirname( __FILE__ ), "overrides" )
+
 module Ream
-  #
   module Blog
-    #
     class TemplateCache
-      #
-      #
-      #
-      def initialize( config )
-        @root_dir = config.templates_root
-        @cache = {}
-        
-        @call_cache = {}
-        
-        scan_templates
+      def initialize( source )
+        @source = source
+        init_cache
+        scan_sources
       end
+      
+      attr_reader :source
 
-      #
-      #
-      #
       def rescan
-        scan_templates
+        scan_sources
       end
 
-      #
-      #
-      #
       def fetch( *names )
-        result = @call_cache[ names ]
-        unless result
-          result = {}
-          names.each do |name|
-            case name
-            when Array  then result.merge!( fetch( *name ) )
-            when String then result.merge!( @cache[ name ] || {} )
-            when Hash   then result.merge!( name )
-            else raise "Unknown argument type for TemplateCache#fetch: #{name.inspect}"
-            end
-          end
-          result = Ream::Template.expand( result )
-          @call_cache[ names ] = result
-        end
-        result
+        @call_cache[ names ] ||= 
+          Ream::Template.expand( fetch0( *names ) ).with( Ream::Blog::TemplateProcessor )
       end
-  
-    protected
-      #
-      def scan_templates
-        @scanner = Ream::Template::Scanner.new
-        Dir.chdir( @root_dir ) do
-          Dir[ "**/*.tpl" ].each do |filename|
-            key = filename[ /^(.*)\.tpl$/, 1 ]
-            @cache[ key ] = @scanner.scan( filename )
-          end
+
+      def each( &block )
+        @source.keys.each do |key|
+          value = fetch( key )
+          block.call( key, value )
         end
+      end
+
+      def files
+        return [] unless @source.is_a?( Ream::Sources::FS )
+        @source.files
+      end
+
+    protected
+
+      def init_cache
+        @cache = {}
+        @call_cache = {}
+      end
+
+      def scanner
+        @scanner ||= Ream::Template::Scanner.new
+      end
+
+      def scan_sources
+        @source.each do |key, value|
+          @cache[ key ] = scanner.scan( value )
+        end
+      end
+
+      def fetch0( *names )
+        names.inject( {} ) do |hash, name|
+          hash.merge( fetch_by_name( name ) )
+        end
+      end
+
+        
+        
+
+      def fetch_by_name( name )
+        case name
+        when Array  then fetch0( *name )
+        when String then ( @cache[ name ] || {} )
+        when Hash   then name
+        else raise unknown_argument( name )
+        end
+      end
+
+      def unknown_argument( name )
+        "Unknown argument type "    \
+        "for TemplateCache#fetch: " \
+        "#{name.inspect}"
       end
     end
   end
